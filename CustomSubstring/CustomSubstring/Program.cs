965ca5d3aeeb9d8382435974a82a8a34b0370c54
nesrines <nasrinfa@code.edu.az>
2023-04-05 23:09:13 +0400

    added CustomSubstring

---
+++ CustomSubstring/CustomSubstring/Program.cs
@@ -0,0 +1,29 @@
+﻿string CustomSubstring(string Str, int StartIndex, int Length)
+{
+    string Result = string.Empty;
+    for (int i = StartIndex; i < StartIndex + Length; i++)
+    { Result += Str[i]; }
+    return Result;
+}
+
+try
+{
+    Console.WriteLine("Enter a string:");
+    string Str = Console.ReadLine();
+    Console.WriteLine("Enter start index:");
+    int StartIndex = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter end index:");
+    int Length = int.Parse(Console.ReadLine());
+
+    try
+    {
+        string Result = CustomSubstring(Str, StartIndex, Length);
+        Console.WriteLine(Result);
+    }
+
+    catch (Exception)
+    { Console.WriteLine("There is no such index in your string."); }
+}
+
+catch (Exception)
+{ Console.WriteLine("Index must be a number."); }
